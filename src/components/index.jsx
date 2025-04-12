1c0bb604f6ae1586015dddca648da418821b37e1
Hey K <heykis00@gmail.com>
2025-04-12 22:24:20 +0530

    file changes added

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,8 @@
 export { Header } from './Common/Header/Header';
 export { Footer } from './Common/Footer/Footer';
-export { GridView } from './GridViesw/GridView';
-export { ListView } from './ListView/ListView';
-export { Wishlist } from './Wishlist/Wishlist';
-export { Cart } from './Cart/Cart';
+export { GridView } from './Common/GridViesw/GridView';
+export { ListView } from './Common/ListView/ListView';
+export { Wishlist } from './Common/Wishlist/Wishlist';
+export { Cart } from './Common/Cart/Cart';
 export { NavigationBar } from './Common/NavigationBar/NavigationBar';
 export { DrawerModel } from './Common/DrawerModel/DrawerModel';
